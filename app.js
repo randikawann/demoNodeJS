15759a1d3fe4fd1db73744706499fe11e5b2352d
randikawann <randikawann@gmail.com>
2018-09-09 18:37:38 -0500

    add Date method as external module

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var http = require('http');
+var dt = require('./count.js');
 
-var server = http.createServer(function(req,res){
-    res.writeHead(200,{'content-Type':'text/plain'});
-    res.end('hello randika');
-});
-
-server.listen(3000, '127.0.0.1');
-console.log('now listening to port 3000');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
+}).listen(3000); 
+console.log("WebPage is runnig on port 3000");
