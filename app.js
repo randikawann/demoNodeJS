50ea616d61efe548239c01544da6ea0476a3e208
randikawann <randikawann@gmail.com>
2018-09-09 19:23:49 -0500

    create new file

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req,res){
+    // read demo_html.html file
     fs.readFile('demo_html.html',function(err,data){
         res.writeHead(200,{'Content-Type':'text/html'});
         res.write(data);
         res.end();
+    });
+    // create demo_txy.html
+    fs.appendFile('demo_txt.html','hello randika',function(err){
+        if(err) throw err;
+        console.log('Saved');
     });
     
 });
