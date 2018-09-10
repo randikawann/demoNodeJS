6a1bd28ac245d152851430ff3d3464116c9ae400
randikawann <randikawann@gmail.com>
2018-09-09 21:13:08 -0500

    use upper case module

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+// add upper-case module using cmd, npm install upper-case
+
 var http = require('http');
+var uc = require('upper-case')
 
 var server = http.createServer(function (req, res) {
- 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(uc('Hello do it'));
+    res.end();
 })
 server.listen(3000,'127.0.0.1'); 
 console.log('now listening to port 3000');
