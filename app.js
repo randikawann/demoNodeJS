7ddd0a66d4e9ca6779656b1928d3a40785ac8599
randikawann <randikawann@gmail.com>
2018-09-09 21:06:26 -0500

    add upper case module

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
 var http = require('http');
-var url = require('url');
-var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+ 
 })
 server.listen(3000,'127.0.0.1'); 
 console.log('now listening to port 3000');
