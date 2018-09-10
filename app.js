f2c178d337d632dc3390da41436f62bc6eed7b6b
randikawann <randikawann@gmail.com>
2018-09-09 20:51:05 -0500

    added 404 for web page

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+var http = require('http');
 var url = require('url');
-var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
-//Parse the address:
-var q = url.parse(adr, true);
+var fs = require('fs');
 
-/*The parse method returns an object containing url properties*/
-console.log(q.host);
-console.log(q.pathname);
-console.log(q.search);
-
-/*The query property returns an object with all the querystring parameters as properties:*/
-var qdata = q.query;
-console.log(qdata.month);
+var server = http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+})
+server.listen(3000,'127.0.0.1'); 
+console.log('now listening to port 3000');
